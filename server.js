da4741200df68c32b6346ceeec1631f6ce67e710
sharaizk <64202206+sharaizk@users.noreply.github.com>
2022-05-29 13:34:47 +0500

    Initial

---
+++ server.js
@@ -0,0 +1,19 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    greet: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => "Hello World",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
